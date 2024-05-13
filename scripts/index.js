c4efd3c83232e1a2c71b3546b2f0063c43955b73
Thomas Wang <77226475+peacebypieces@users.noreply.github.com>
2024-05-12 23:56:18 -0400

    used javascript to add experience elements

--- scripts/index.js
+++ scripts/index.js
@@ -2,30 +2,75 @@ const exps = [
     {
         name: "Alphabet Inc",
         dateRange: "2017-2018",
-        description: "yadda yadda yadda yadda"
+        description: ""
 
     },
     {
         name: "lol",
         dateRange: "2020-2011",
         description: "lol",
+    },
+    {
+        name: "Software Engineer Intern",
+        dateRange: "Warner Robins Air Force Base (2022-2024) ",
+        description: "burrito man"
     }
 ]
 
-const timeline = document.getElementById("experiences");
+const timeline = document.getElementById("experienceslist");
 
-for (let i = 0; i < exps.length; i++) {
+for (let i = exps.length - 1; i >= 0; i--) {
+
+    if (i == exps.length - 1) {
+        const expContent = `   
+            <li class="mb-10 ms-6">            
+                <span class="absolute flex items-center justify-center w-6 h-6 bg-blue-100 rounded-full -start-3 ring-8 ring-white dark:ring-gray-900 dark:bg-blue-900">
+                    <svg class="w-2.5 h-2.5 text-blue-800 dark:text-blue-300" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+                        <path d="M20 4a2 2 0 0 0-2-2h-2V1a1 1 0 0 0-2 0v1h-3V1a1 1 0 0 0-2 0v1H6V1a1 1 0 0 0-2 0v1H2a2 2 0 0 0-2 2v2h20V4ZM0 18a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V8H0v10Zm5-8h10a1 1 0 0 1 0 2H5a1 1 0 0 1 0-2Z"/>
+                    </svg>
+                </span>
+                <h3 class="flex items-center mb-1 text-lg font-semibold text-white"> ${exps[i].name}<span class="bg-blue-100 text-blue-800 text-sm font-medium me-2 px-2.5 py-0.5 rounded dark:bg-blue-900 dark:text-blue-300 ms-3">Present</span></h3>
+                <time class="block mb-2 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">${exps[i].dateRange}</time>
+                <p class="mb-4 text-base font-normal text-gray-500 dark:text-gray-400">${exps[i].description}</p>
+            </li>
+        `
+        timeline.innerHTML += expContent;
+
+        continue;
+    } else if (i == 0) {
+
+            const expContent = `   
+            <li class="ms-6">            
+                <span class="absolute flex items-center justify-center w-6 h-6 bg-blue-100 rounded-full -start-3 ring-8 ring-white dark:ring-gray-900 dark:bg-blue-900">
+                    <svg class="w-2.5 h-2.5 text-blue-800 dark:text-blue-300" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+                        <path d="M20 4a2 2 0 0 0-2-2h-2V1a1 1 0 0 0-2 0v1h-3V1a1 1 0 0 0-2 0v1H6V1a1 1 0 0 0-2 0v1H2a2 2 0 0 0-2 2v2h20V4ZM0 18a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V8H0v10Zm5-8h10a1 1 0 0 1 0 2H5a1 1 0 0 1 0-2Z"/>
+                    </svg>
+                </span>
+                <h3 class="flex items-center mb-1 text-lg font-semibold text-white"> ${exps[i].name}</h3>
+                <time class="block mb-2 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">${exps[i].dateRange}</time>
+                <p class=" text-base font-normal text-gray-500 dark:text-gray-400">${exps[i].description}</p>
+            </li>
+        `
+        timeline.innerHTML += expContent;
+
+        continue;
+
+    }
 
     const expContent = `
-    <div class="relative flex-1 pt-2 w-1/2 left-0">
-        <div class="text-black bg-white relative border-4 rounded border-white">
-            <h2 class="font-black text-4xl pl-3 pt-3">${exps[i].name}</h2>
-            <small class="text-gray-800 pl-3">${exps[i].dateRange}</small>
-            <p class="pl-3 pb-3">${exps[i].description}</p>
-        </div>
-    </div>
+    <li class="mb-10 ms-6">            
+        <span class="absolute flex items-center justify-center w-6 h-6 bg-blue-100 rounded-full -start-3 ring-8 ring-white dark:ring-gray-900 dark:bg-blue-900">
+            <svg class="w-2.5 h-2.5 text-blue-800 dark:text-blue-300" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+                <path d="M20 4a2 2 0 0 0-2-2h-2V1a1 1 0 0 0-2 0v1h-3V1a1 1 0 0 0-2 0v1H6V1a1 1 0 0 0-2 0v1H2a2 2 0 0 0-2 2v2h20V4ZM0 18a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V8H0v10Zm5-8h10a1 1 0 0 1 0 2H5a1 1 0 0 1 0-2Z"/>
+            </svg>
+        </span>
+        <h3 class="flex items-center mb-1 text-lg font-semibold text-white"> ${exps[i].name}</h3>
+        <time class="block mb-2 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">${exps[i].dateRange}</time>
+        <p class="mb-4 text-base font-normal text-gray-500 dark:text-gray-400">${exps[i].description}</p>
+    </li>
     `
-
     timeline.innerHTML += expContent;
+
+    
 }
 
